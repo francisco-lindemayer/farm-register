809844ff2afe84638ead017aec26cef6d532e266
francisco-lindemayer <francisco.lindemayer@gmail.com>
2022-05-23 20:32:58 -0300

    Validator identitycode length

--- src/producer/dto/create-producer.dto.ts
+++ src/producer/dto/create-producer.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { MaxLength, MinLength } from 'class-validator';
 
 export class CreateProducerDto {
+  @MinLength(11)
+  @MaxLength(14)
   @ApiProperty({
     description: 'CPF ou CNPJ',
     example: '99999999999 ou 44444444000144',
